68f6dea01ba0e5b6845b0e8c07dcba214fab4d27
ncharming <17323473313@163.com>
2020-11-23 21:27:35 +0800

    redis学习分支

---
+++ think-in-redis/src/test/java/org/practice/redis/RedisApplicationTests.java
@@ -0,0 +1,13 @@
+package org.practice.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
