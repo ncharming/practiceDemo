f83ac8fab3f98dc27e70a09576c022a93c12771d
290006 <huowenjie_ah@bonc.com.cn>
2020-12-02 18:24:53 +0800

    redis 实战-登录次数限制

--- think-in-redis/src/main/java/org/practice/redis/practiceReal/SignInLimit.java
+++ think-in-redis/src/main/java/org/practice/redis/practiceReal/SignInLimit.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.websocket.server.PathParam;
+import java.time.Duration;
 
 /**
  * @Author :nihongyu
@@ -16,13 +17,15 @@ import javax.websocket.server.PathParam;
  */
 @Service
 public class SignInLimit {
+	static int tempTimesRemain = 0;
 
 	@Autowired
 	private RedisTemplate<String, Object> redisTemplate;
 
 
 	public ReturnObject sign(String username, String password) {
-		int tempTimesRemain = 0;
+
+		boolean isSign = true;
 		int timesRemain = 5;//还可登录的余下次数
 		ReturnObject returnObject = new ReturnObject();
 
@@ -36,7 +39,7 @@ public class SignInLimit {
 			returnObject.setRespInfo("登录成功");
 			return returnObject;
 		} else {
-			if (tempTimesRemain == 0) {//第一次登录(如何判定是第一次登录？？)
+			if (tempTimesRemain == 0) {//第一次登录(如何判定是第一次登录？？，) tempTimesRemain 设置成 static，在类加载的时候就加载，不会变化
 				redisTemplate.opsForValue().set("timesRemain", timesRemain);
 			}
 			tempTimesRemain++;
@@ -44,6 +47,11 @@ public class SignInLimit {
 			returnObject.setRespCode("1");
 			returnObject.setRespDesc("fail");
 			returnObject.setRespInfo("登录失败，还有" + times + "次机会");
+
+			if (times == 0) {//锁定，半小时之后才能登录
+
+//				redisTemplate.opsForValue().getOperations().expire("expireTime",);
+			}
 		}
 
 		return returnObject;
